a9f6576e04ffaadeeb3aac2218486b32501462d1
Vamsidharan-V <vamsidharan.1902249@srec.ac.in>
2021-09-24 12:59:33 +0530

    Bugs fixed and cabin drawer made

--- Assets/Scripts/StartTextLv1.cs
+++ Assets/Scripts/StartTextLv1.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class StartTextLv1 : MonoBehaviour
 {
     public GameObject InitialText;
+    public AudioSource InitAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         InitialText.SetActive(false);
+        InitAudio.Stop();
         
     }
 
@@ -26,8 +29,10 @@ public class StartTextLv1 : MonoBehaviour
     IEnumerator InitT()
     {
         yield return new WaitForSeconds(5);
+        InitAudio.Play();
         InitialText.SetActive(true);
         yield return new WaitForSeconds(5);
+        InitAudio.Stop();
         InitialText.SetActive(false);
     }
 }
